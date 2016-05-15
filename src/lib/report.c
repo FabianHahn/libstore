a95df47d6e02d4d08834d6cf42b6d1126d7e66cc
Fabian Hahn <fabian@hahn.graphics>
2016-05-15 17:58:14 +0200

    added report generation

---
+++ src/lib/report.c
@@ -0,0 +1,42 @@
+#include <stddef.h> // NULL
+#include <string.h> // strdup
+#include "report.h"
+#include "types.h"
+
+static void appendParseReport(StoreParseReport *report, StoreDynamicString reportString, int level);
+static void appendNTimes(StoreDynamicString string, int n, const char *what);
+
+char *StoreGenerateParseReport(StoreParser *parser)
+{
+	StoreDynamicString reportString = StoreCreateDynamicString();
+	appendParseReport(parser->state.lastReport, reportString, 0);
+
+	char *result = strdup(StoreReadDynamicString(reportString));
+	StoreFreeDynamicString(reportString);
+	return result;
+}
+
+static void appendParseReport(StoreParseReport *report, StoreDynamicString reportString, int level)
+{
+	if(report == NULL) {
+		return;
+	}
+
+	appendParseReport(report->previousReport, reportString, level);
+
+	appendNTimes(reportString, level, "\t");
+	if(report->success) {
+		StoreAppendDynamicString(reportString, "successfully parsed %s at line %d, column %d: %s\n", report->type, report->position.line, report->position.column, report->message);
+	} else {
+		StoreAppendDynamicString(reportString, "failed to parse %s at line %d, column %d: %s\n", report->type, report->position.line, report->position.column, report->message);
+	}
+
+	appendParseReport(report->lastSubReport, reportString, level + 1);
+}
+
+static void appendNTimes(StoreDynamicString string, int n, const char *what)
+{
+	for(int i = 0; i < n; i++) {
+		StoreAppendDynamicString(string, what);
+	}
+}
---
+++ src/lib/report.h
@@ -0,0 +1,8 @@
+#ifndef LIBSTORE_REPORT_H
+#define LIBSTORE_REPORT_H
+
+#include "parser.h"
+
+char *StoreGenerateParseReport(StoreParser *parser);
+
+#endif
